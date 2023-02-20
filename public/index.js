1a4f40a8cf2be1b817cfba93e4a38bdda614fd7b
Anestis Nt <113776405+AnestisNt@users.noreply.github.com>
2023-02-20 13:55:42 +0000

    test10

--- public/index.js
+++ public/index.js
@@ -4,3 +4,12 @@ const button1 = document.getElementById('button1');
 button1.addEventListener('click', () =>{
   h1.textContent = 'sup dawg';
 });
+
+const myImage = document.querySelector('img');
+
+myImage.onclick = () =>{
+  const src = myImage.getAttribute('src');
+  src === "../public/images/image1.jpg"
+  ? myImage.setAttribute('src', "../public/images/image2.jpg")
+  : myImage.setAttribute('src', "../public/images/image1.jpg");
+};
